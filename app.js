254d1b527385a6234be64bafa7049cf50e8032fa
iiakash03 <iiakash59@gmail.com>
2023-02-05 20:34:42 +0530

    sending response to different request

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
-    console.log("Akash");
+    let url=req.url;
+    if(url==='/home'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>')
+        res.write('<head><title>first nodejs</title></head>');
+        res.write('<body>Welcome home</body>')
+        res.write('</html>');
+        res.end();
+    }else if(url==='/about'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>')
+        res.write('<head><title>first nodejs</title></head>');
+        res.write('<body>Welcome to about us page</body>')
+        res.write('</html>');
+        res.end();
+    }else if(url==='/node'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>')
+        res.write('<head><title>first nodejs</title></head>');
+        res.write('<body>Welcome to my node js project</body>')
+        res.write('</html>');
+        res.end();
+    }
 })
 
 server.listen(4000);
