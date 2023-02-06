30aa949cdb2cd76ff1535fb8f1263e7274149ffb
iiakash03 <iiakash59@gmail.com>
2023-02-06 08:43:19 +0530

    fetching data from file

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 const http=require('http');
+const fs=require('fs');
+const { Stream } = require('stream');
 const server=http.createServer((req,res)=>{
     let url=req.url;
-    if(url==='/home'){
+    let method=req.method;
+    if(url==='/'){
+
+        fs.readFile('message.txt',{ encoding:"utf-8"},(err,data)=>{
+
         res.setHeader('Content-Type','text/html');
         res.write('<html>')
         res.write('<head><title>first nodejs</title></head>');
-        res.write('<body>Welcome home</body>')
+        res.write(`<body>${data}</body>`)
+        res.write('<body><form action="/message" method="POST"><input name="message"></input><input type="submit"></input>></form></body>')
         res.write('</html>');
-        res.end();
-    }else if(url==='/about'){
-        res.setHeader('Content-Type','text/html');
-        res.write('<html>')
-        res.write('<head><title>first nodejs</title></head>');
-        res.write('<body>Welcome to about us page</body>')
-        res.write('</html>');
-        res.end();
-    }else if(url==='/node'){
-        res.setHeader('Content-Type','text/html');
-        res.write('<html>')
-        res.write('<head><title>first nodejs</title></head>');
-        res.write('<body>Welcome to my node js project</body>')
-        res.write('</html>');
-        res.end();
+        return res.end();
+    })
+    }
+    if(url==='/message' && method==='POST'){
+        const body=[];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        })
+
+        req.on('end',()=>{
+            const parsedBody=Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message=parsedBody.split('=')[1];
+            fs.writeFile('message.txt',message,(err)=>{
+                res.statusCode=302;
+            res.setHeader('Location','/');
+            return res.end();
+
+            });
+            
+            
+        })
+        
+        
     }
 })
 
