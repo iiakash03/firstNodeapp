6090bc3f885743edfac4b17b7c4a85616ef1baea
iiakash03 <iiakash59@gmail.com>
2023-02-08 10:06:32 +0530

    express first commit

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const http=require('http');
-const handle=require('./script');
+const express=require('express')
 
-const server=http.createServer(handle);
+const app=express();
+
+app.use((req,res,next)=>{
+    console.log("In the middleware");
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log("In another middleware")
+    res.send('<h1>Hello World</h1>');
+});
+
+const server=http.createServer(app);
    
 server.listen(4000);
