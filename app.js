8418846dc795a4c7305046a7a6323eb6c22b4c62
iiakash03 <iiakash59@gmail.com>
2023-02-08 19:26:14 +0530

    express js

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ const express=require('express')
 
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log("In the middleware");
-    next();
-})
+const bodyparser=require('body-parser')
 
-app.use((req,res,next)=>{
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.use('/add-product',(req,res,next)=>{
     console.log("In another middleware")
-    res.send('<h1>Hello World</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">add product</button></form>');
 });
 
-const server=http.createServer(app);
-   
-server.listen(4000);
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
+    console.log("This always run")
+    next();
+});
+
+
+
+app.listen(4000);
