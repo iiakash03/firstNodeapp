ab38f1c01540a397b029db2050e3fffde2c46442
iiakash03 <iiakash59@gmail.com>
2023-02-09 16:52:06 +0530

    serving html css js

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const express=require('express')
 
+const path=require('path');
+
+
+
 const routeadmin=require('./routes/admin');
 
 const routeshop=require('./routes/shop');
 
 const app=express();
 
+app.use(express.static(path.join(__dirname,'public')))
+
 const bodyparser=require('body-parser')
 
 app.use(routeadmin);
 app.use(routeshop);
 
+
+
+app.use('/admin',routeadmin);
+
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
 
 app.use(bodyparser.urlencoded({extended:true}));
+
+
 
 app.listen(4000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,34 @@
 const express=require('express');
 
+const path=require('path');
+
 const router=express.Router();
 
 const bodyparser=require('body-parser')
 
 router.get('/add-product',(req,res,next)=>{
    // console.log("In another middleware")
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">add product</button></form>');
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'))
 });
 
-router.post('/product',(req,res,next)=>{
+router.post('/add-product',(req,res,next)=>{
     console.log(req.body);
     res.redirect('/');
 })
+
+router.get('/contactus',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'../','views','contact.html'))
+})
+
+router.post('/contactus',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect("/success");
+})
+
+router.get('/success',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'../','views','success.html'))
+})
+
+
 
 module.exports=router;
